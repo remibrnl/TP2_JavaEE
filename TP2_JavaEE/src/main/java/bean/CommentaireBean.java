fa82c536e6b345687016addc7887fedd5c9ff18b
lucas <@lucas.moreau@etu.univ-tours.fr>
2021-05-25 14:31:45 +0100

    cc

--- TP2_JavaEE/src/main/java/bean/CommentaireBean.java
+++ TP2_JavaEE/src/main/java/bean/CommentaireBean.java
@@ -1,5 +1,19 @@
 package bean;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class CommentaireBean {
+	@Id
+	private Long idCommentaire;
+	
+	private String contenu;
+	
+	private Long nbLikes;
+	
+	@ManyToOne
+	private Long idMembre;
+	
 
 }
