c9487cb8f89aa940e67a732bfcb408678ba1febc
Rémi B <7360391+remibrnl@users.noreply.github.com>
2021-05-25 14:36:45 +0100

    getter & setters

--- TP2_JavaEE/src/main/java/bean/CommentaireBean.java
+++ TP2_JavaEE/src/main/java/bean/CommentaireBean.java
@@ -14,6 +14,46 @@ public class CommentaireBean {
 	
 	@ManyToOne
 	private Long idMembre;
-	
 
+	public CommentaireBean(Long idCommentaire, String contenu, Long nbLikes, Long idMembre) {
+		super();
+		this.idCommentaire = idCommentaire;
+		this.contenu = contenu;
+		this.nbLikes = nbLikes;
+		this.idMembre = idMembre;
+	}
+
+	public Long getIdCommentaire() {
+		return idCommentaire;
+	}
+
+	public void setIdCommentaire(Long idCommentaire) {
+		this.idCommentaire = idCommentaire;
+	}
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	public Long getNbLikes() {
+		return nbLikes;
+	}
+
+	public void setNbLikes(Long nbLikes) {
+		this.nbLikes = nbLikes;
+	}
+
+	public Long getIdMembre() {
+		return idMembre;
+	}
+
+	public void setIdMembre(Long idMembre) {
+		this.idMembre = idMembre;
+	}
+	
+	
 }
--- TP2_JavaEE/src/main/java/bean/MembreBean.java
+++ TP2_JavaEE/src/main/java/bean/MembreBean.java
@@ -16,4 +16,55 @@ public class MembreBean {
 	private String email;
 	
 	private String hashMdp;
+
+	public MembreBean(Long idMembre, String nom, String prenom, String email, String hashMdp) {
+		super();
+		this.idMembre = idMembre;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.hashMdp = hashMdp;
+	}
+
+	public Long getIdMembre() {
+		return idMembre;
+	}
+
+	public void setIdMembre(Long idMembre) {
+		this.idMembre = idMembre;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getHashMdp() {
+		return hashMdp;
+	}
+
+	public void setHashMdp(String hashMdp) {
+		this.hashMdp = hashMdp;
+	}
+	
+	
 }
