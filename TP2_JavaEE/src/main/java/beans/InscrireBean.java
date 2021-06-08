0b8a504335f652a41fcab280f81d56b70af0c737
WilliamBes <williambesnard1@gmail.com>
2021-06-08 14:41:46 +0200

    css on

--- TP2_JavaEE/src/main/java/beans/InscrireBean.java
+++ TP2_JavaEE/src/main/java/beans/InscrireBean.java
@@ -2,10 +2,8 @@ package beans;
 
 import java.io.Serializable;
 
-import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
